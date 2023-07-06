77f97b3fee63d7c55e8ba1420b6b35b7d557f08a
J <72083559+jay002200@users.noreply.github.com>
2023-07-06 16:55:00 +0800

    Update shopee_coin.py

--- shopee_coin.py
+++ shopee_coin.py
@@ -5,8 +5,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import time
-user = "jay002200@gmail.com"
-pwd = "zxc484375"
+user = "*"
+pwd = "*"
 
 
 
